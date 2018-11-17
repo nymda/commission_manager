21d3c4f4ab15d4c4bd8fa2bf131198b8db7aef01
jake hartley <puffleboy101@hotmail.co.uk>
2018-11-17 21:23:20 +0000

    -my brain hurts

--- feef_commission_thing/Form1.Designer.cs
+++ feef_commission_thing/Form1.Designer.cs
@@ -29,12 +29,231 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sketched = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Finished = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Remove = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Price,
+            this.Sketched,
+            this.Finished,
+            this.Remove});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(619, 426);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            this.Name.Width = 175;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            // 
+            // Sketched
+            // 
+            this.Sketched.HeaderText = "Sketched";
+            this.Sketched.Name = "Sketched";
+            this.Sketched.ReadOnly = true;
+            // 
+            // Finished
+            // 
+            this.Finished.HeaderText = "Finished";
+            this.Finished.Name = "Finished";
+            this.Finished.ReadOnly = true;
+            // 
+            // Remove
+            // 
+            this.Remove.HeaderText = "Remove";
+            this.Remove.Name = "Remove";
+            this.Remove.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(640, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(279, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(680, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(637, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(641, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Price:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(680, 35);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "TODO: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "DONE:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "DONE (%): 0%";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "MONEY:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(640, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 78);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Info";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(928, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "commission manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sketched;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Finished;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Remove;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- feef_commission_thing/Form1.cs
+++ feef_commission_thing/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,262 @@ namespace feef_commission_thing
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string datpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/comminfo.dat";
+        public List<string> data = new List<String> { };
+        public int money;
+        public int todo;
+        public int done;
+        public int total;
+        public float percentage;
+        public int startcounter;
+
+        public void updateStats()
+        {
+            todo = 0;
+            done = 0;
+            total = 0;
+            money = 0;
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if(dataGridView1.Rows.Count == 0)
+                {
+                    label3.Text = "TODO: 0";
+                    label4.Text = "DONE: 0";
+                    label5.Text = "DONE (%): 0%";
+                    label6.Text = "MONEY: £0";
+                }
+
+                Console.WriteLine("2 " + (Convert.ToBoolean(dataGridView1.Rows[i].Cells[2].Value)));
+                Console.WriteLine("3 " + (Convert.ToBoolean(dataGridView1.Rows[i].Cells[3].Value)));
+                if (Convert.ToBoolean(dataGridView1.Rows[i].Cells[3].Value) == false)
+                {
+                    Console.WriteLine("found false");
+                    todo++;
+                }
+                if (Convert.ToBoolean(dataGridView1.Rows[i].Cells[3].Value) == true)
+                {
+                    done++;
+                }
+                total++;
+
+                string[] o = data[i].Split(':');
+
+                money = money + Convert.ToInt32(o[1]);
+
+                Console.WriteLine(todo);
+                Console.WriteLine(done);
+                Console.WriteLine(total);
+
+                label3.Text = "TODO: " + todo.ToString();
+                label4.Text = "DONE: " + done.ToString();
+
+                if (!(done == 0))
+                {
+                    label5.Text = "DONE (%): " + ((done * 100 / total * 100) / 100).ToString() + "%"; //maff
+                    percentage = done / total;
+                    Console.WriteLine(percentage);
+                }
+
+                label6.Text = "MONEY: £" + money;
+            }
+
+
+            Console.WriteLine("\n\n\n\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[0].Name = "Name";
+            dataGridView1.Columns[1].Name = "Price";
+            string[] row = new string[] { textBox1.Text, numericUpDown1.Value.ToString() };
+            dataGridView1.Rows.Add(row);
+
+            DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
+            DataGridViewCheckBoxColumn chk2 = new DataGridViewCheckBoxColumn();
+            dataGridView1.Columns.Add(chk);
+            dataGridView1.Columns.Add(chk2);
+            chk.HeaderText = "Sketched";
+            chk2.HeaderText = "Complete";
+            chk.Name = "chk";
+            chk2.Name = "chk2";
+
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            dataGridView1.Columns.Add(btn);
+            btn.HeaderText = "Remove";
+            btn.Text = "Remove";
+            btn.Name = "btn";
+            btn.UseColumnTextForButtonValue = true;
+
+            data.Add(textBox1.Text + ":" + numericUpDown1.Value.ToString() + ":" + "False" + ":" + "False");
+
+            File.WriteAllLines(datpath, data);
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                string[] o = data[i].Split(':');
+
+                if (o[2] == "True")
+                {
+                    Console.WriteLine(o[2]);
+                    dataGridView1.Rows[i].Cells[2].Value = true;
+                }
+                if (o[2] == "False")
+                {
+                    dataGridView1.Rows[i].Cells[2].Value = false;
+                }
+
+                if (o[3] == "True")
+                {
+                    dataGridView1.Rows[i].Cells[3].Value = true;
+                }
+                if (o[3] == "False")
+                {
+                    dataGridView1.Rows[i].Cells[3].Value = false;
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show((e.RowIndex + 1) + "  Row  " + (e.ColumnIndex + 1) + "  Column button clicked ");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            //i seriously have no fucking clue whats going on here
+
+            try
+            {
+                if(e.ColumnIndex == 4)
+                {
+                    dataGridView1.Rows.RemoveAt(e.RowIndex);
+                    data.RemoveAt(e.RowIndex);
+                    File.WriteAllLines(datpath, data);
+                }
+                if (e.ColumnIndex == 3)
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = !(Convert.ToBoolean(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value));
+                    string[] tdata = data[e.RowIndex].Split(':');
+                    tdata[3] = (Convert.ToBoolean(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value).ToString());
+                    data[e.RowIndex] = string.Join(":", tdata);
+                    File.WriteAllLines(datpath, data);
+
+                }
+                if (e.ColumnIndex == 2)
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = !(Convert.ToBoolean(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value));
+                    string[] tdata = data[e.RowIndex].Split(':');
+                    tdata[2] = (Convert.ToBoolean(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value).ToString());
+                    data[e.RowIndex] = string.Join(":", tdata);
+                    File.WriteAllLines(datpath, data);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+
+
+            if (File.Exists(datpath))
+            {
+                data = File.ReadAllLines(datpath).ToList();
+            }
+
+            foreach (string i in data)
+            {
+                startcounter++;
+                startcounter = startcounter - 1;
+                dataGridView1.ColumnCount = 2;
+                dataGridView1.Columns[0].Name = "Name";
+                dataGridView1.Columns[1].Name = "Price";
+
+                string[] o = i.Split(':');
+
+                string[] row = new string[] { o[0], o[1], "", "" };
+
+                dataGridView1.Rows.Add(row);
+
+                DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
+                DataGridViewCheckBoxColumn chk2 = new DataGridViewCheckBoxColumn();
+                dataGridView1.Columns.Add(chk);
+                dataGridView1.Columns.Add(chk2);
+                chk.HeaderText = "Sketched";
+                chk2.HeaderText = "Complete";
+                chk.Name = "chk";
+                chk2.Name = "chk2";
+
+                DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+                dataGridView1.Columns.Add(btn);
+                btn.HeaderText = "Remove";
+                btn.Text = "Remove";
+                btn.Name = "btn";
+                btn.UseColumnTextForButtonValue = true;
+            }
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (Convert.ToBoolean(dataGridView1.Rows[i].Cells[2].Value) == false)
+                {
+                    todo++;
+                }
+                if (Convert.ToBoolean(dataGridView1.Rows[i].Cells[3].Value) == true)
+                {
+                    done++;
+                }
+                total++;
+
+                label3.Text = todo.ToString();
+                label4.Text = done.ToString();
+
+                if (!(done == 0))
+                {
+                    label5.Text = "DONE (%) " + ((done * 100 / total * 100) / 100).ToString() + "%"; //maff
+                    percentage = done / total;
+                    Console.WriteLine(percentage);
+                }
+
+                string[] o = data[i].Split(':');
+
+                if (o[2] == "True")
+                {
+                    Console.WriteLine(o[2]);
+                    dataGridView1.Rows[i].Cells[2].Value = true;
+                }
+                if (o[2] == "False")
+                {
+                    dataGridView1.Rows[i].Cells[2].Value = false;
+                }
+
+                if (o[3] == "True")
+                {
+                    dataGridView1.Rows[i].Cells[3].Value = true;
+                }
+                if (o[3] == "False")
+                {
+                    dataGridView1.Rows[i].Cells[3].Value = false;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            updateStats();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
